74e0e83059916a0c4f798fb746e44fc2323f01d6
Hadeed Ahmad <hadeedji@gmail.com>
2024-07-15 17:47:00 +0500

    Partial work

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,18 @@
 export default {
-  content: ["index.html"]
+  content: ["index.html"],
+  theme: {
+    colors: {
+      blue: {
+        200: "#8BACD9",
+        400: "#2E405A",
+        600: "#15263F",
+        800: "#0D192C",
+      },
+      cyan: "#00FFF8",
+      white: "#FFFFFF",
+    },
+    fontFamily: {
+      sans: ["Outfit", "sans-serif"],
+    },
+  },
 };
